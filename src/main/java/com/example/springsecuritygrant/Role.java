27274ae8aed2e30efd82642803cb06e6b50ebfd1
KirillovItstep <kirillov.malp@gmail.com>
2022-06-01 20:04:49 +0300

    Initial commit

---
+++ src/main/java/com/example/springsecuritygrant/Role.java
@@ -0,0 +1,15 @@
+package com.example.springsecuritygrant;
+
+import lombok.Data;
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name="roles")
+public class Role {
+    @Id
+    @Column(name="role_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
---
+++ src/main/java/com/example/springsecuritygrant/SpringSecurityGrantApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecuritygrant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityGrantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityGrantApplication.class, args);
+    }
+
+}
